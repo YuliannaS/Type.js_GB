d7cfb4a68e1ea4092be510ddf7ebd24c614b7d05
Yulianna Sandler <yuli.sandler@gmail.com>
2022-11-17 12:36:43 +0400

    Task-2

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-import { renderSearchFormBlock } from './search-form.js'
-import { renderSearchStubBlock } from './search-results.js'
-import { renderUserBlock } from './user.js'
-import { renderToast } from './lib.js'
+import { renderSearchFormBlock } from './search-form.js';
+import { renderSearchStubBlock } from './search-results.js';
+import { renderUserBlock } from './user.js';
+import { renderToast, getUserData, getFavoritesAmount } from './lib.js';
+
+
+localStorage.setItem('user', JSON.stringify({
+  username: 'Yulianna Sandler',
+  avatarUrl:'../img/avatar.png'
+}))
+
+localStorage.setItem('favoritesAmount', '9');
+
+const { username, avatarUrl } = JSON.parse(getUserData('user'));
+const favoritesAmount: number = +getFavoritesAmount('favoritesAmount');
+
 
 window.addEventListener('DOMContentLoaded', () => {
   renderUserBlock('Yulianna Sandler', '/img/avatar.png', 1);
@@ -12,3 +24,24 @@ window.addEventListener('DOMContentLoaded', () => {
       {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
   )
 })
+
+
+interface SearchFormData {
+  checkIn: string;
+  checkOut: string;
+  maxСost: number;
+}
+function processingSearch(): SearchFormData{
+  console.log('processingSearch');
+  const searchdata: SearchFormData={
+    checkIn: string;
+    checkOut: string;
+    maxСost: umber;
+  }
+  };
+  function search (searchData: SearchFormData) {
+    if (searchData != null) {
+      console.log(searchData);
+    }
+  }
+  
